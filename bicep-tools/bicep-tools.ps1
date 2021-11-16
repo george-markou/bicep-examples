edf9d7f476c73d7590b6f2294a75bcc2cf1e54e5
George Markou <george_markou@hotmail.com>
2021-11-16 16:58:57 +0200

    added bicep-tools.ps1

---
+++ bicep-tools/bicep-tools.ps1
@@ -0,0 +1,17 @@
+## Install Visual Studio Code ##
+Winget install Microsoft.VisualStudioCode
+## Install Bicep CLI and Tools ##
+Winget install Microsoft.Bicep
+## Install latest version of PowerShell ##
+Winget install PowerShell
+## Install AzureCLI ##
+Winget install Microsoft.AzureCLI
+## Install Az PowerShell Module ##
+Install-Module -name Az
+## Install Git ##
+Winget install Git.Git
+## Install GitHub CLI ##
+Winget install GitHub.cli
+## Install required Visual Studio Extensions - reload PowerShell Session  ##
+code --install-extension ms-vscode.vscode-node-azure-pack
+code --install-extension github.vscode-pull-request-github
